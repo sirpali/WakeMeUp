2b0e869666f05ca0e6adf93ace3d414ae4bad68d
Sander van't Veer <sander.vantveer@gmail.com>
2012-07-18 02:08:47 +0200

    First commit

---
+++ src/com/sirpali/wakemeup/MainActivity.java
@@ -0,0 +1,30 @@
+package com.sirpali.wakemeup;
+
+
+import com.sirpali.wakemeup.*;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.support.v4.app.NavUtils;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        TextView mainText = (TextView) findViewById(R.id.maintext);
+        mainText.setText("Fuck yeah");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    
+}
